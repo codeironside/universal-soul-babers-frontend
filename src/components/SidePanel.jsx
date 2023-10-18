ebbe24b3c3b56092021efd50d01abffa8aa93f62
Bee-code-tech <babawaleolatunji64@gmail.com>
2023-10-18 10:52:57 +0100

    contd...

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -13,7 +13,7 @@ const SidePanel = () => {
           <p className='text-para mt-0 font-semibold text-headingColor ' >Working Hours: </p>
           <ul className="mt-3">
             <li className="flex items-center justify-between mb-2">
-                <p className='text-[15px]  leading-6 text-textColor font-semibold' >Sunday</p>
+                <p className='text-[15px]  leading-6 text-textColor  font-semibold' >Sunday</p>
                 <p className='text-[15px]  leading-6 text-textColor font-semibold' >4:00 PM - 10:00 PM</p>
             </li>
             <li className="flex items-center justify-between mb-2">
