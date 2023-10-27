67b7f9fd1438a1e283448232dd1b6c92e0968047
Bee-code-tech <babawaleolatunji64@gmail.com>
2023-10-27 23:34:33 +0100

    barbershop

--- src/components/BarberShop.jsx
+++ src/components/BarberShop.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const BarberShop = () => {
+const BarberShop = ({}) => {
   return (
     <section className='container mx-auto -mt-[50px] ' > 
-     <div className="flex bg-yellow-200 text-yellow-500 items-start justify-start">
+     <div className="flex bg-yellow-50 text-yellow-500 items-start justify-start">
       <p>You have no shop yet. select a pricing plan that fits your budget or create one for free now</p>
      </div>
     </section>
