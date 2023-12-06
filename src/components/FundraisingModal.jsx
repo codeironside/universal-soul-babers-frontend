7e500f558922d216ddb7038a4ba630cafa5df242
unknown <chukwukareuben09@gmail.com>
2023-12-06 14:40:30 +0000

    integrated the chat forum api

--- src/components/FundraisingModal.jsx
+++ src/components/FundraisingModal.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { buildApiEndpoint, getCookie } from "../utils"
 
 const FundraisingModal = ({ setModalDisplay }) => {
+  const [formData, setFormData] = useState({
+    campaignName: '',
+    goalAmount: '',
+    description: '',
+    startDate: '',
+    endDate: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  
+  const token = getCookie('token');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      // Make the Axios request with the appropriate headers, including the cookie
+      const response = await axios.post(
+        'https://unique-barbers.onrender.com/api/v1/chats/getall',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${userCookie}`, // Include the cookie in the Authorization header
+          },
+        }
+      );
+
+      // Handle the response as needed
+      console.log('Response:', response.data);
+    } catch (error) {
+      // Handle errors
+      console.error('Error:', error.message);
+    }
+  };
+
   return (
      <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none px-4">
         <div className="relative lg:w-4/12 w-11/12 my-6 mx-auto">
           <div
+            style={{ overflowY: 'auto' }}
             className="border-0 h-96 py-6 md:px-20 px-10 rounded-lg shadow-lg relative flex flex-col bg-white w-full outline-none focus:outline-none"
           >
             {/*body*/}
@@ -17,18 +60,56 @@ const FundraisingModal = ({ setModalDisplay }) => {
             </h1>
           
             <input
-              type="text"
+             type="text"
+             name="campaignName"
+              value={formData.campaignName}
+              onChange={handleChange}
+              placeholder="Campaign Name"
               style={{ border: "1px solid rgba(224, 229, 237, 1)" }}
-              placeholder="Name Your Campaign"
               className="w-full mb-4 py-3 rounded-xl"
             />
 
               <input
-              type="text"
+             type="text"
+             name="goalAmount"
+              value={formData.goalAmount}
+              onChange={handleChange}
+              placeholder="Goal Amount"
               style={{ border: "1px solid rgba(224, 229, 237, 1)" }}
-              placeholder="Set your Fundraise Goal"
               className="w-full mb-4 py-3 rounded-xl"
             />
+
+           <input
+             type="text"
+             name="description"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Description"
+              style={{ border: "1px solid rgba(224, 229, 237, 1)" }}
+              className="w-full mb-4 py-3 rounded-xl"
+            />
+
+            <input
+              type="date"
+              name="startDate"
+              value={formData.startDate}
+              onChange={handleChange}
+              placeholder="Start Date"
+              style={{ border: "1px solid rgba(224, 229, 237, 1)" }}
+              className="w-full mb-4 py-3 rounded-xl"
+            />
+
+             <input
+              type="date"
+              name="endDate"
+              value={formData.endDate}
+              onChange={handleChange}
+              placeholder="End Date"
+              style={{ border: "1px solid rgba(224, 229, 237, 1)" }}
+              className="w-full mb-4 py-3 rounded-xl"
+            />
+
+
             <button
               type="button"
               className="w-full py-3 rounded-xl text-white mb-4"
