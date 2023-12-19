3669bfc1a429dd0af56601c5e47b5702fdac9f5f
Bee-code-tech <babawaleolatunji64@gmail.com>
2023-12-19 12:20:09 +0100

    fix: text edit for shop

--- src/pages/MyShop.jsx
+++ src/pages/MyShop.jsx
@@ -207,10 +207,11 @@ export default function MyShop() {
           </h1>
 
           <button
-            type='button'
-            className='inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm w-fit bg-primaryDark focus:outline-none focus:ring-2 focus:ring-primaryDark focus:ring-offset-2'
-            onClick={() => setOpenAddProduct(true)}>
-            <PlusIcon className='w-5 h-5 mr-2 -ml-1' aria-hidden='true' />
+            type="button"
+            className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm w-fit bg-primaryDark focus:outline-none focus:ring-2 focus:ring-primaryDark focus:ring-offset-2"
+            onClick={() => setOpenAddProduct(true)}
+          >
+            <PlusIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
             Add Shop
           </button>
 
