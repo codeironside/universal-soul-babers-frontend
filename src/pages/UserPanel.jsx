5bd067687cbe42e843a8dad5aa68b2586a129623
Bee-code-tech <babawaleolatunji64@gmail.com>
2023-12-19 11:32:42 +0100

    fix: added home url to dashboard

--- src/pages/UserPanel.jsx
+++ src/pages/UserPanel.jsx
@@ -320,6 +320,19 @@ export default function UserPanel({ fragment, owner = false }) {
                           <Menu.Item>
                             {({ active }) => (
                               <Link
+                                to="/"
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                Home
+                              </Link>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Link
                                 to="/profile"
                                 className={classNames(
                                   active ? 'bg-gray-100' : '',
