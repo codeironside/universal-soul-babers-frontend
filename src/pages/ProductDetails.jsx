6bf66386029e7e093c1fc57c82d8a37200eb8953
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-01-01 23:39:21 +0100

    Update ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,63 +1,3 @@
-// import React, { useContext } from 'react'
-// import { useParams } from "react-router-dom";
-// import { CartContext } from "../context/CartContext";
-// import { ProductContext } from "../context/ProductContext";
-
-// const ProductDetails = () => {
-//   // get products from id 
-//   const { id: itemId } = useParams()
-//   const { productItem } = useContext(ProductContext)
-//   const { addToCart } = useContext(CartContext)
-
-
-//   // change the product id to number 
-//   const itemIdNumber = parseInt(itemId, 10);
-//   // get single product 
-//   const product = productItem.find(item => {
-//     if (item.id === itemIdNumber) {
-//       return item
-//     }
-//   })
-
-//   if (!product) {
-//     return (
-//       <section className='h-screen flex justify-center items-center' >
-//         loading...
-//       </section>
-//     )
-//   }
-
-//   // console.log(productItem.find(item => console.log(item.id)));
-
-//   // destructure products 
-//   const { shop_name, price, description, images } = product
-//   console.log(img);
-
-//   return (
-//     <section className='pt-32 lg:py-32 h-screen flex items-center ' >
-//       <div className='container mx-auto'>
-//         {/* images wrapper  */}
-//         <div className='flex flex-col lg:flex-row items-center' >
-//           {/* images  */}
-//           <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0  ' >
-//             <img className='max-w-[200px] lg:max-w-sm rounded-xl ' src={img} alt="" />
-//           </div>
-//           {/* text  */}
-//           <div className='flex-1 text-center lg:text-left' >
-//             <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto  lg:mx-0 ' >{title}</h1>
-//             <div className='text-xl text-red-500 font-medium mb-6 ' >$ {price}</div>
-//             <p className='mb-6' >{content}</p>
-//             <button onClick={() => addToCart(product, product.id)} className='btn' >Add To Cart </button>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default ProductDetails
-
-
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
@@ -89,7 +29,19 @@ const ProductDetails = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row items-center'>
           <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
-            <img className='max-w-[200px] lg:max-w-sm rounded-xl' src={images} alt='' />
+                 {images ? (
+        <img
+          className='w-full group-hover:scale-110 transition duration-300 '
+          src={images}
+          alt=''
+        />
+      ) : (
+        <img
+          className='w-full group-hover:scale-110 transition duration-300 '
+          src={placeholderImage}
+          alt='Placeholder'
+        />
+      )}
           </div>
           <div className='flex-1 text-center lg:text-left'>
             <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{shop_name}</h1>
