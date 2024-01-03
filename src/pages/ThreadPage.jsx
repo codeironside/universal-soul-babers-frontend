19fe3bdc1592ad123a71e5b4a65f2a2b0c69759d
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-01-03 16:42:35 +0100

    Update ThreadPage.jsx

--- src/pages/ThreadPage.jsx
+++ src/pages/ThreadPage.jsx
@@ -103,7 +103,7 @@ import Comment from "../components/Forum/Comments";
 import { getCookie } from "../utils";
 import axios from "axios";
 
-const token = getCookie('token');
+const token = getCookie("token");
 
 const ThreadPage = () => {
   const [newComment, setNewComment] = useState("");
@@ -118,32 +118,35 @@ const ThreadPage = () => {
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
+
+      console.log(response.data);
       setThread(response.data.thread);
       setComments(response.data.comments);
     } catch (error) {
-      console.error('Error fetching chats:', error);
+      console.error("Error fetching chats:", error);
     }
   };
-  
+
   const handleSubmit = async () => {
     try {
       const response = await axios.post(
         `https://unique-barbers.onrender.com/api/v1/threads/thread/${id}`,
-        { 
-          content: newComment, 
+        {
+          content: newComment,
           image: "Owner Name",
         },
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
+
       console.log("Comment created successfully:", response.data);
       fetchOneThread();
       setNewComment("");
@@ -152,87 +155,70 @@ const ThreadPage = () => {
     }
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchOneThread();
-  },[])
+  }, []);
+
+  const getInitial = () => {
+    if (thread?.image) {
+      return null; // Return null if image is available
+    } else {
+      return thread?.userName.charAt(0).toUpperCase(); // Return the first letter of userName
+    }
+  };
 
   return (
-    <div className="container mx-auto p-4">
-      
-      <div className="flex items-center justify-center mb-2 relative">
-        <div className="image-container" style={{ width: '600px', height: '60px', position: 'relative' }}>
-
+    <div className="container mx-auto p-4 flex flex-col items-center">
+      <div className="bg-gray-200 rounded-lg p-6 max-w-xl w-full relative">
+        <div className="absolute top-0 left-0 right-0 bg-dark-gold h-1/2 z-10 rounded-full border-4 border-transparent overflow-hidden">
+          <div className="absolute w-full h-full top-0 left-0 transform -rotate-45 bg-purple-800 bg-gradient-to-b from-purple-800 via-transparent to-transparent"></div>
+          <div className="absolute w-full h-full top-0 left-0 transform rotate-45 bg-dark-gold bg-gradient-to-b from-dark-gold via-transparent to-transparent"></div>
+        </div>
+        <div className="relative z-20">
           {thread?.image ? (
             <img
-              className="w-16 h-16 rounded-full object-cover z-10"
-              src={thread?.image}
-              alt={thread?.userName}
+              src={thread?.image} // Add image source here
+              alt="Thread Image"
+              className="rounded-full border-4 border-gray-300 animate-pulse w-32 h-32 object-cover mx-auto"
             />
           ) : (
-            <div className="w-16 h-16 rounded-full bg-gray-300 flex items-center justify-center text-gray-600 font-bold text-2xl z-10">
-              {thread?.userName ? thread.userName.charAt(0) : ''}
+            <div className="flex items-center justify-center rounded-full border-4 border-gray-300 animate-pulse w-32 h-32 bg-gray-100 text-gray-600 text-2xl font-semibold">
+              {getInitial()}
             </div>
           )}
-          <div 
-            className="broken-border" 
-            style={{
-                position: 'absolute',
-                top: '0',
-                left: '0',
-                width: '100%',
-                height: '100%',
-                borderRadius: '50%',
-                border: '15px dashed transparent',
-                animation: 'moveBorder 10s linear infinite',
-                animationDelay: '1s', // Add delay to animation
-              }}
-          ></div>
         </div>
-                  <p className="text-gray-600 text-sm absolute bottom-0 left-0 ml-2">
-            Posted by {thread?.userName} on {thread?.createdAt}
-          </p>
+        <h1 className="text-3xl font-bold text-center mt-4">{thread?.topic}</h1>
+        <p className="text-gray-600 text-sm mb-2 text-center">
+          Posted by {thread?.userName} on {thread?.createdAt}
+        </p>
+        <p className="text-gray-800 text-center">{thread?.thread}</p>
       </div>
-        <div className="card-content mt-4">
-          <p className="text-gray-800">{thread?.thread}</p>
-          <p className="text-gray-600 text-sm absolute bottom-0 left-0 ml-2">
-            Posted by {thread?.userName} on {thread?.createdAt}
-          </p>
-        </div>
-      <div className="mt-4">
-        <h2 className="text-lg font-semibold mb-2">Comments</h2>
-        {comments?.map((comment) => (
-          <Comment key={comment.id} {...comment} />
-        ))}
-        <div className="mt-4">
-          <textarea
-            className="w-full p-2 border rounded"
-            placeholder="Add a new comment..."
-            value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
-          />
-          <button
-            onClick={handleSubmit}
-            className="mt-2 bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-          >
-            Add Comment
-          </button>
+      <div className="mt-4 flex items-center justify-center relative">
+        <div className="w-1/2 h-0.5 bg-gray-400 absolute top-0 z-0" />
+        <div className="bg-white p-4 rounded-lg shadow-lg max-w-xl w-full z-10">
+          <h2 className="text-lg font-semibold mb-2">Comments</h2>
+          {comments?.map((comment) => (
+            <Comment key={comment.id} {...comment} />
+          ))}
+          <div className="mt-4">
+            <textarea
+              className="w-full p-2 border rounded"
+              placeholder="Add a new comment..."
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+            />
+            <button
+              onClick={handleSubmit}
+              className="mt-2 bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+            >
+              Add Comment
+            </button>
+          </div>
         </div>
       </div>
-      <style>
-        {`
-          @keyframes moveBorder {
-            0%, 100% {
-              border-image: linear-gradient(to right, #ffd700, #ffd700 25%, #b38e00 25%, #b38e00 50%, #998000 50%, #998000 75%, transparent 75%, transparent);
-            }
-            50% {
-              border-image: linear-gradient(to right, #ffd700, #ffd700 25%, #b38e00 25%, #b38e00 50%, #998000 50%, #998000 75%, #ffd700 75%, #ffd700);
-            }
-          }
-          /* Additional styles or adjustments can be made as needed */
-        `}
-      </style>
     </div>
   );
 };
 
 export default ThreadPage;
+
