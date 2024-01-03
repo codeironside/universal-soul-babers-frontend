3bdb20460b266c301d97b981705983ac5e8b25fd
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-01-03 23:36:35 +0100

    Update ThreadPage.jsx

--- src/pages/ThreadPage.jsx
+++ src/pages/ThreadPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useRed, useEffect } from "react";
+import React, { useState,useRef, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Comment from "../components/Forum/Comments";
 import { getCookie } from "../utils";
