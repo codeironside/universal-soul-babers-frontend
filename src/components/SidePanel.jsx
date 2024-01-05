5c3e12a0b566eb341de50eba9d6b2a44c5c7e889
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-01-05 13:49:10 +0100

    Update SidePanel.jsx

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -38,56 +38,54 @@ const SidePanel = ({ setModalShow, data }) => {
     };
     return defaultHours;
   };
-const workingHours = data?.whours || {};
-console.log(workingHours)
+const { hours = {} } = data || {};
 
-  return (
-    <div className='shadow-panelShadow p-4 lg:p-5 rounded-md'>
-      <div className='flex items-center justify-between'>
-        <p className='text-para mt-0 font-semibold'>Booking Price</p>
-        <span className='text-[16px] lg:text-[20px] lg:leading-6 text-headingColor font-bold'>
-          $ {data ? `${data.price}` : "not available"}
-        </span>
-      </div>
-      
-      {/* Working hours */}
-      <div className='mt-[30px] p-4'>
-        <p className='text-para mt-0 font-semibold text-headingColor'>
-          Working Hours:
-        </p>
-        <ul className='mt-3'>
-{Object.entries(workingHours).map(([day, times]) => {
-  if (Array.isArray(times) && times.length > 0) {
-    return (
-      <li key={day} className='flex flex-col w-full mb-2'>
-        <p className='text-[15px] leading-6 text-textColor font-semibold'>
-          {day}
-        </p>
-        <div className='flex flex-wrap'>
-          {times.map((timeSlot, index) => (
-            <span
-              key={index}
-              className='text-[13px] lg:text-[15px] leading-6 text-textColor bg-gray-200 rounded-md px-2 py-1 mr-2 mb-2'
-            >
-              {timeSlot}
-            </span>
-          ))}
-        </div>
-      </li>
-    );
-  }
-})}
-   </ul>
-      </div>
-      <button
-        onClick={() => setModalShow(true)}
-        className='btn px-2 w-full rounded-md'
-      >
-        Hire Me
-      </button>
+return (
+  <div className='shadow-panelShadow p-4 lg:p-5 rounded-md'>
+    <div className='flex items-center justify-between'>
+      <p className='text-para mt-0 font-semibold'>Booking Price</p>
+      <span className='text-[16px] lg:text-[20px] lg:leading-6 text-headingColor font-bold'>
+        $ {data ? `${data.price}` : "not available"}
+      </span>
     </div>
-  );
-};
+    
+    {/* Working hours */}
+    <div className='mt-[30px] p-4'>
+      <p className='text-para mt-0 font-semibold text-headingColor'>
+        Working Hours:
+      </p>
+      <ul className='mt-3'>
+        {Object.entries(hours).map(([day, times]) => {
+          if (Array.isArray(times) && times.length > 0) {
+            return (
+              <li key={day} className='flex flex-col w-full mb-2'>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                  {day}
+                </p>
+                <div className='flex flex-wrap'>
+                  {times.map((timeSlot, index) => (
+                    <span
+                      key={index}
+                      className='text-[13px] lg:text-[15px] leading-6 text-textColor bg-gray-200 rounded-md px-2 py-1 mr-2 mb-2'
+                    >
+                      {timeSlot}
+                    </span>
+                  ))}
+                </div>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+    <button
+      onClick={() => setModalShow(true)}
+      className='btn px-2 w-full rounded-md'
+    >
+      Hire Me
+    </button>
+  </div>
+);
 //   return (
 //     <div className='shadow-panelShadow p-4 lg:p-5 rounded-md'>
 //       <div className='flex items-center justify-between'>
