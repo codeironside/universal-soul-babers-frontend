2498fffee4cd50f5ec210029ef1e8a0df87ae794
Bee-code-tech <babawaleolatunji64@gmail.com>
2024-01-05 23:47:14 +0100

    jj

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,6 +3,10 @@ import { useParams, Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 import { ProductContext } from "../context/ProductContext";
 import img from '../assets/img/product-2.jpg'
+
+const ProductDetails = () => {
+  const { id: itemId } = useParams();
+  const { productItem } = useContext(ProductContext);
   const { addToCart } = useContext(CartContext);
   const [product, setProduct] = useState(null);
 
